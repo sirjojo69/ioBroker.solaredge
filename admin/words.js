d9b29c31b56425b32cecbe36bde9377de97d0a0d
root <root@iobrokerTest.fritz.box>
2019-08-24 16:15:05 +0000

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'solaredge adapter settings': {
+		'en': 'Adapter settings for solaredge',
+		'de': 'Adaptereinstellungen für solaredge',
+		'ru': 'Настройки адаптера для solaredge',
+		'pt': 'Configurações do adaptador para solaredge',
+		'nl': 'Adapterinstellingen voor solaredge',
+		'fr': "Paramètres d'adaptateur pour solaredge",
+		'it': "Impostazioni dell'adattatore per solaredge",
+		'es': 'Ajustes del adaptador para solaredge',
+		'pl': 'Ustawienia adaptera dla solaredge',
+		'zh-cn': 'solaredge的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};
